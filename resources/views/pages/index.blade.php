81b6eb4f400f261e13be89ac110fdd58d6e48a28
Daniel <daniel.vernberg@gmail.com>
2016-10-12 20:47:37 +0200

    using brand button and muted text

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,26 +5,26 @@
   <div class="container">
     <div class="row">
       <div class="col-md-8 col-md-offset-2">
-        <h2>Popular</h2>
+        <h2 class="text-muted">Popular</h2>
         @include('partials.palette-loop', [
           'palettes' => $popular,
         ])
         <br>
         <div class="text-center">
-          <a href="{{ route('popular') }}" class="btn btn-primary">View all popular</a>
+          <a href="{{ route('popular') }}" class="btn btn-brand">View all popular</a>
         </div>
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-8 col-md-offset-2">
-        <h2>Recent</h2>
+        <h2 class="text-muted">Recent</h2>
         @include('partials.palette-loop', [
           'palettes' => $recent,
         ])
         <br>
         <div class="text-center">
-          <a href="{{ route('recent') }}" class="btn btn-primary">View all recent</a>
+          <a href="{{ route('recent') }}" class="btn btn-brand">View all recent</a>
         </div>
       </div>
     </div>
