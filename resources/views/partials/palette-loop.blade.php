c10dc74092287998bfee8d1f55c9830139421c83
Daniel <daniel.vernberg@gmail.com>
2016-10-12 19:53:40 +0200

    no need for rows

--- resources/views/partials/palette-loop.blade.php
+++ resources/views/partials/palette-loop.blade.php
@@ -1,15 +1,14 @@
 @foreach ($palettes as $palette)
-  <div class="row">
-    <div class="palette">
-      <div class="palette-colors">
-        <div class="box" style="background-color: {{$palette->color1}}"></div>
-        <div class="box" style="background-color: {{$palette->color2}}"></div>
-        <div class="box" style="background-color: {{$palette->color3}}"></div>
-        <div class="box" style="background-color: {{$palette->color4}}"></div>
-      </div>
-      <div class="palette-info">
-        <p>Created by <a href="{{ route('profile', $palette->user->id) }}">{{ $palette->user->name }}</a></p>
-      </div>
+  <div class="palette">
+    <div class="palette-colors">
+      <div class="box" style="background-color: {{$palette->color1}}"></div>
+      <div class="box" style="background-color: {{$palette->color2}}"></div>
+      <div class="box" style="background-color: {{$palette->color3}}"></div>
+      <div class="box" style="background-color: {{$palette->color4}}"></div>
+    </div>
+    <div class="palette-info">
+      <p>Created by <a href="{{ route('profile', $palette->user->id) }}">{{ $palette->user->name }}</a></p>
+      <p class="text-muted">{{ $palette->created_at->diffForHumans() }}</p>
     </div>
   </div>
 @endforeach
