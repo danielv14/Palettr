4b1dcbf61b2b64e9d058ea5ccda41150fdbf79c3
Daniel <daniel.vernberg@gmail.com>
2016-10-13 09:33:07 +0200

    menu text instead of hamburger icon

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -4,9 +4,7 @@
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+        <span class="menu">Menu</span>
       </button>
       <a class="navbar-brand" href="/">Palettr</a>
     </div>
