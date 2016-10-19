4c1efe014184f3d7a5179c121414183ca54bfb9c
Daniel <daniel.vernberg@gmail.com>
2016-10-19 10:33:50 +0200

    custom 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Page not found | Palettr</title>
+  <link href="/css/app.css" rel="stylesheet">
+
+</head>
+<body>
+
+  <section class="hero hero-full">
+
+    <div class="hero-content-wrapper">
+      <div class="hero-text">
+        <h1>404</h1>
+        <p>We couldn't find the page you are looking for</p>
+        <br>
+        <a href="/" class="btn btn-brand btn-lg">Take me home</a>
+      </div>
+    </div>
+
+  </section>
+</body>
+</html>
